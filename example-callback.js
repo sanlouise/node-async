ffff82043b1d33d5ee90bbf0b5d38f509617f69e
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-05-30 18:12:44 +0200

    Move weather retrieving logic to module

--- example-callback.js
+++ example-callback.js
@@ -1,20 +1,6 @@
-var request = require('request');
-var url = 'http://api.openweathermap.org/data/2.5/weather?appid=3e0246cd6661e75446a7625568010b08&q=Soestdijk&units=metric';
 //Require module. The ./ ensure we are in the same dir as this file.
 var weather = require('./weather.js');
 
-weather();
-
-request({
-	url: url,
-	json: true
-}, function (error, response, body) {
-	if (error) {
-		console.log('Unable to find weather for this location.');
-	} else {
-		//Print what is in the body. The 4 stands for the number of spaces by which JSON is indented.
-		console.log(JSON.stringify(body, null, 4));
-		console.log('It is ' + body.main.temp + ' degrees Celcius in ' + body.name);
-
-	}
+weather(function (currentWeather) {
+	console.log(currentWeather);
 });
--- weather.js
+++ weather.js
@@ -1,4 +1,21 @@
-module.exports = function() {
+var request = require('request');
+var url = 'http://api.openweathermap.org/data/2.5/weather?appid=3e0246cd6661e75446a7625568010b08&q=Soestdijk&units=metric';
 
-	console.log('Got weather!');
+
+module.exports = function(callback) {
+
+	request({
+		url: url,
+		json: true
+	}, function (error, response, body) {
+		if (error) {
+			callback('Unable to find weather for this location.');
+		} else {
+			//Print what is in the body. The 4 stands for the number of spaces by which JSON is indented.
+			// console.log(JSON.stringify(body, null, 4));
+			callback('It is ' + body.main.temp + ' degrees Celcius in ' + body.name);
+
+		}
+	});
 }
+
